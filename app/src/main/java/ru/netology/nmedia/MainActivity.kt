90991a9165f95700f28246a2006382aaa78974ed
Elena01001 <jamely1@yandex.ru>
2022-05-07 19:29:29 +0300

    Fifth Commit

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -2,42 +2,41 @@ package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.annotation.DrawableRes
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewModel.PostViewModel
 
 const val THOUSAND = 1000
 const val MILLION = THOUSAND * THOUSAND
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: PostViewModel by viewModels() // делегирование
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-            likedByMe = false,
-            shared = false
-        )
+        // повесили наблюдателя за отрисовкой поста, как только в посте изменятся данные,
+        // то вызовется лямбда с ф-цией рендера, кот создана ниже
+        viewModel.data.observe(this) { post -> binding.render(post) }
 
-        binding.render(post)
-
+        // повесили слушателя за нажатием лайка, те нажали лайк, во вьюмодели вызовется метод
+        // onLikeClicked, кот в свое время вызовет из репозитория метод like, like получит
+        // текущий пост, лайкнет/дизлайкнет его и обновит данные в liveData
+        // liveData - это живой поток, в кот только одни какие-то данные, самая актуальная последняя инфа.
+        // Вызывая метод value - мы закидываем данные в поток, на кот кто-то где-то подписывается, напр наша viewModel
+        // на поле data подписалась наша activity, поэтому данные обновились в liveData и вызвался перерендеринг
         binding.postLayout.likeButton.setOnClickListener {
-            post.likedByMe = !post.likedByMe
-            binding.postLayout.likeButton.setImageResource(getLikeIconResId(post.likedByMe))
-            if (post.likedByMe) post.likes++ else post.likes--
-            binding.postLayout.likeNumbers.text = showNumberView(post.likes)
+            viewModel.onLikeButtonClicked()
         }
 
         binding.postLayout.shareButton.setOnClickListener {
-            binding.postLayout.shareButton.setImageResource(R.drawable.ic_baseline_share_24)
-            binding.postLayout.shareNumbers.text = showNumberView(post.shares)
-            if (post.shared) post.shares else post.shares++
+            viewModel.onShareButtonClicked()
         }
     }
 
@@ -62,8 +61,10 @@ class MainActivity : AppCompatActivity() {
             in THOUSAND..9999 -> {
                 var numberQuantity = String.format("%.1f", (currentNumber).toDouble() / THOUSAND)
                 if (numberQuantity.endsWith(",0")) {
-                    numberQuantity = numberQuantity.substring(0,
-                        numberQuantity.length -2)
+                    numberQuantity = numberQuantity.substring(
+                        0,
+                        numberQuantity.length - 2
+                    )
                 }
                 numberQuantity + "K"
             }
@@ -74,8 +75,10 @@ class MainActivity : AppCompatActivity() {
             else -> {
                 var numberQuantity = String.format("%.1f", (currentNumber).toDouble() / MILLION)
                 if (numberQuantity.endsWith(",0")) {
-                    numberQuantity = numberQuantity.substring(0,
-                        numberQuantity.length -2)
+                    numberQuantity = numberQuantity.substring(
+                        0,
+                        numberQuantity.length - 2
+                    )
                 }
                 numberQuantity + "M"
             }
