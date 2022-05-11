8912a8b4a36e914ffce4daf5593dccec9ab65097
Elena01001 <jamely1@yandex.ru>
2022-05-11 20:12:28 +0300

    Sixth Commit

--- app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
+++ app/src/main/java/ru/netology/nmedia/viewModel/PostViewModel.kt
@@ -3,11 +3,12 @@ package ru.netology.nmedia.viewModel
 import androidx.lifecycle.ViewModel
 import ru.netology.nmedia.data.PostRepository
 import ru.netology.nmedia.data.impl.InMemoryPostRepository
+import ru.netology.nmedia.dto.Post
 
 class PostViewModel : ViewModel() {
 
     private val repository: PostRepository = InMemoryPostRepository()
     val data get() = repository.data
-    fun onLikeButtonClicked() = repository.like()
-    fun onShareButtonClicked() = repository.share()
+    fun onLikeButtonClicked(post: Post) = repository.like(post.id)
+    fun onShareButtonClicked(post: Post) = repository.share(post.id)
 }
