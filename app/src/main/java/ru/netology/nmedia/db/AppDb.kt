6f5ffd6b114da721a387a0da0daab75718f79126
Elena01001 <jamely1@yandex.ru>
2022-07-05 13:38:41 +0300

    Twenty-second Commit

--- app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -2,13 +2,21 @@ package ru.netology.nmedia.db
 
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 // Ключевая идея: мы хотим получить синглтон на все приложение для доступа к БД (через метод getInstance),
 // Это некая симуляция нашей БД, кот принимает на вход другую БД
 // Когда мы создадим экземпляр AppDb, то получим доступ к постДао и создадим репозиторий
+// Это некая точка входа в Room
 
-class AppDb private constructor(db: SQLiteDatabase) {
-    val postDao: PostDao = PostDaoImpl(db)
+@Database(
+    entities = [PostEntity::class],
+    version = 1
+)
+abstract class AppDb : RoomDatabase() {
+    abstract val postDao: PostDao
 
     companion object {
         @Volatile
@@ -16,14 +24,15 @@ class AppDb private constructor(db: SQLiteDatabase) {
 
         fun getInstance(context: Context): AppDb {
             return instance ?: synchronized(this) {
-                instance ?: AppDb(
-                    buildDatabase(context, arrayOf(PostsTable.DDL))
-                ).also { instance = it }
+                instance ?: buildDatabase(context)
+                    .also { instance = it }
             }
         }
 
-        private fun buildDatabase(context: Context, DDLs: Array<String>) = DbHelper(
-            context, 1, "app.db", DDLs,
-        ).writableDatabase
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context, AppDb::class.java, "app.db" // имя файла, куда созданная БД сохранится
+            ).allowMainThreadQueries() // разрешить обрабатывать запросы на основном потоке
+                .build()
     }
 }
--- app/src/main/java/ru/netology/nmedia/db/Posts.kt
+++ app/src/main/java/ru/netology/nmedia/db/Posts.kt
@@ -5,17 +5,29 @@ import ru.netology.nmedia.dto.Post
 
 // делаем ф-цию расширения на курсоре
 
-fun Cursor.toPost() = Post(
-    // мы не можем из курсора вытащить данные по названию колонки, а только по ее ид(или индексу)
-    id = getLong(getColumnIndexOrThrow(PostsTable.Column.ID.columnName)),
-    author = getString(getColumnIndexOrThrow(PostsTable.Column.AUTHOR.columnName)),
-    content = getString(getColumnIndexOrThrow(PostsTable.Column.CONTENT.columnName)),
-    published = getString(getColumnIndexOrThrow(PostsTable.Column.PUBLISHED.columnName)),
-    likes = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKES.columnName)),
-    // если нам придет инт, кот != 0, то запишется в true, все, что == 0, то запишется в false
-    likedByMe = getInt(getColumnIndexOrThrow(PostsTable.Column.LIKED_BY_ME.columnName)) != 0,
-    shares = getInt(getColumnIndexOrThrow(PostsTable.Column.SHARES.columnName)),
-    shared = getInt(getColumnIndexOrThrow(PostsTable.Column.SHARED.columnName)) != 0,
-    viewings = getInt(getColumnIndexOrThrow(PostsTable.Column.VIEWINGS.columnName)),
-    videoLink = getString(getColumnIndexOrThrow(PostsTable.Column.VIDEOLINK.columnName))
+fun PostEntity.toModel() = Post(
+    // мы не можем вытащить данные по названию колонки, а только по ее ид(или индексу)
+    id = id,
+    author = author,
+    content = content,
+    published = published,
+    likes = likes,
+    likedByMe = likedByMe,
+    shares = shares,
+    shared = shared,
+    viewings = viewings,
+    videoLink = videoLink
+)
+
+fun Post.toEntity() = PostEntity(
+    id = id,
+    author = author,
+    content = content,
+    published = published,
+    likes = likes,
+    likedByMe = likedByMe,
+    shares = shares,
+    shared = shared,
+    viewings = viewings,
+    videoLink = videoLink
 )
